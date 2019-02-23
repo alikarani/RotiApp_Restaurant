73e6ca76a54b1785914c0241dfab2e8440326790
aliasgharkarani <aliasgharkarani125@hotmail.com>
2019-02-23 10:51:00 +0500

    Res data get to check payments

---
+++ ThisWeekIncome.js
@@ -0,0 +1,119 @@
+// ThisWeekIncome
+
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Image,
+    View,
+    Dimensions,
+    TouchableOpacity,
+    TextInput,
+    ScrollView
+} from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+const { height, width, fontScale } = Dimensions.get('window');
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
+        'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
+});
+export default class InProcessOrders extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checking: this.props.navigation.state.params.ordered1,
+            filtered1: []
+        }
+        this.Get = this.Get.bind(this);
+        this.Accepted = this.Accepted.bind(this);
+    }
+    UNSAFE_componentWillMount() {
+        this.Get();
+    }
+    Get() {
+        fetch(`https://rotiappserver.herokuapp.com/api/restaurants/${this.state.checking}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then(function (response) {
+            return response.json();
+        }).then(data => {
+            this.setState({
+                filtered1: data
+            })
+        }
+        ).catch(error => alert("No Orders"));
+    }
+    // Accepted(data) {
+    //     fetch(`https://rotiappserver.herokuapp.com/api/orders/${data.Id}/${this.state.checking}/DeliveryStarted`, {
+    //         method: "PUT",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //     }).then(function (response) {
+    //     }).then(data => this.Get()
+    //     ).catch(error => alert(error));
+    // }
+    render() {
+        return (
+            <ScrollView>
+                <Container>
+                    <Content>
+                        {this.state.filtered1.map((data, i) => {
+                            return (
+                                <Card style={{ flex: 0 }}>
+                                    <CardItem>
+                                        <Left>
+                                            <Body>
+                                                <Text>
+                                                    Order Status: {data.ResOrdStatus}
+                                                </Text>
+                                                <Text>
+                                                    Order Details: {data.OrderDetails}
+                                                </Text>
+                                            </Body>
+                                        </Left>
+                                    </CardItem>
+                                    <CardItem>
+                                        <Body>
+                                            {data.OrderItems && data.OrderItems.map((d, i) => {
+                                                return (
+                                                    <Text>Name:{d.name} - Quantity:{d.quantity} - Price:{d.price}</Text>
+                                                )
+                                            })}
+                                        </Body>
+                                    </CardItem>
+                                    <CardItem>
+                                        <Left>
+                                            <Button textStyle={{ color: '#87838B' }} onPress={() => this.Accepted(data)} >
+                                                <Text>Delivered to Driver</Text>
+                                            </Button>
+                                        </Left>
+                                    </CardItem>
+                                </Card>
+                            )
+                        })}
+                    </Content>
+                </Container>
+            </ScrollView>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    }
+});
